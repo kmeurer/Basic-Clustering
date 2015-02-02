6228903ca1c253810b3c96dd5a28b17fef0c7cfe
Kevin Meurer <kevin.a.meurer@gmail.com>
2015-02-02 13:15:48 -0500

    write default constructor for point class.

--- src/point.java
+++ src/point.java
@@ -1,8 +1,13 @@
 
 public class point {
-
+	private int x; //
+	private int y; 
+	// default constructor sets point to (0, 0)
 	public point() {
-		// TODO Auto-generated constructor stub
+		x = 0;
+		y = 0;
 	}
+	// more common constructor
+	
 
 }
