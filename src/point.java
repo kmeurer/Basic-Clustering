c08fbad2c18b7e60abb7b60f57391129ba87fec6
Kevin Meurer <kevin.a.meurer@gmail.com>
2015-02-02 13:23:45 -0500

    Create constructors for point class.

--- src/point.java
+++ src/point.java
@@ -1,13 +1,18 @@
 
 public class point {
-	private int x; //
-	private int y; 
+	private int x; // x-value
+	private int y; // y-value
+	
 	// default constructor sets point to (0, 0)
 	public point() {
 		x = 0;
 		y = 0;
 	}
-	// more common constructor
 	
+	// more common constructor takes an x and a y value
+	public point(int xVal, int yVal){
+		x = xVal;
+		y = yVal;
+	}
 
 }
